ab17922e068397f66dda99af77e28828ba122871
chalaka78 <nbachalaka@gmail.com>
2021-08-24 12:05:20 +0530

    first commit

---
+++ frontend/src/screens/SingleNote/CreateNote.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import MainScreen from "../../components/MainScreen";
+import { Button, Card, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { createNoteAction } from "../../actions/notesActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
+import ReactMarkdown from "react-markdown";
+
+function CreateNote({ history }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState("");
+
+  const dispatch = useDispatch();
+
+  const noteCreate = useSelector((state) => state.noteCreate);
+  const { loading, error, note } = noteCreate;
+
+  console.log(note);
+
+  const resetHandler = () => {
+    setTitle("");
+    setCategory("");
+    setContent("");
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createNoteAction(title, content, category));
+    if (!title || !content || !category) return;
+
+    resetHandler();
+    history.push("/mynotes");
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <MainScreen title="Make a Reservation">
+      <Card>
+        <Card.Header>Make a new Reservation</Card.Header>
+        <Card.Body>
+          <Form onSubmit={submitHandler}>
+            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+            <Form.Group controlId="title">
+              <Form.Label>Room</Form.Label>
+              <Form.Control
+                type="title"
+                value={title}
+                placeholder="Select the room"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="content">
+              <Form.Label>Date</Form.Label>
+              <Form.Control
+                as="textarea"
+                value={content}
+                placeholder="Select the date"
+                rows={4}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </Form.Group>
+            {content && (
+              <Card>
+                <Card.Header>Note Preview</Card.Header>
+                <Card.Body>
+                  <ReactMarkdown>{content}</ReactMarkdown>
+                </Card.Body>
+              </Card>
+            )}
+
+            <Form.Group controlId="content">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="content"
+                value={category}
+                placeholder="Enter the Category"
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </Form.Group>
+            {loading && <Loading size={50} />}
+            <Button type="submit" variant="primary">
+               Reserve
+            </Button>
+            <Button className="mx-2" onClick={resetHandler} variant="danger">
+              Reset Feilds
+            </Button>
+          </Form>
+        </Card.Body>
+
+        <Card.Footer className="text-muted">
+          Reserve on - {new Date().toLocaleDateString()}
+        </Card.Footer>
+      </Card>
+    </MainScreen>
+  );
+}
+
+export default CreateNote;
