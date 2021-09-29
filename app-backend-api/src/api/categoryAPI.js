7abf44e962fb6aab9ff41dcf8cda59da5eaa5ee6
Yasho96 <trisp7777@gmail.com>
2021-09-29 12:08:43 +0530

    get rooms for categories - backend

--- app-backend-api/src/api/categoryAPI.js
+++ app-backend-api/src/api/categoryAPI.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/categoryController');
 module.exports = function () {
     router.post('/create',controller.createCategory); //referring the function
     router.get('/',controller.getAllCategories);
-    //router.get('/:id',controller.getRoomsForCategories);
+    router.get('/:id',controller.getRoomsForCategories);
     //router.get('/amount/:id',controller.calculateAmount);
     return router;
 };
--- app-backend-api/src/controllers/categoryController.js
+++ app-backend-api/src/controllers/categoryController.js
@@ -24,11 +24,21 @@ const getAllCategories = async (req, res) => {
     }
 }
 
-
+const getRoomsForCategories = async (req, res) => {
+    if (req.params && req.params.id) {        
+        const category = await Category.findById(req.params.id).populate('rooms', 'name description')
+        .then(data => {
+            res.status(200).send({rooms: data.rooms})  //pass data as the response to the frontend
+        })
+        .catch(error => {
+            res.status(500).send({error: error.message});
+        });
+    }
+}
 
 module.exports = {
     createCategory,
     getAllCategories,
-    //getRoomsForCategories,
+    getRoomsForCategories,
     //calculateAmount
 };
